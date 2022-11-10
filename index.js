f28cedccf4ef37cd83b0f835abd4ebb11b719e78
Alex Lee <hyunho98@gmail.com>
2022-11-10 17:23:30 -0500

    commit tree 9a19e5ede1ba3ba59d0be1f3fe03ba4fb4c34c8d

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+        name: name,
+        email: email,
+    };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(formData),
+    };
+
+    return fetch('http://localhost:3000/users', configurationObject)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            const element = document.querySelector('script');
+            element.append(object.id);
+        })
+        .catch(function (error) {
+            const element = document.querySelector('script');
+            element.append(error.message)
+        });
+}
